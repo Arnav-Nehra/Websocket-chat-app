840fba8d87a436a777aa30ff06f3c1887617278b
Arnav-Nehra <arnavnehra1@gmail.com>
2025-07-31 23:36:07 +0530

    signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,11 @@ import Body from "./components/Landing-body";
 import Navbar from "./components/Navbar";
 import Features from "./components/FeaturesComp";
 import Footer from "./components/footer";
+import SignIn from "./components/Signin/SignInCard";
 function App() {
   return (
     <div className=" flex flex-col min-h-screen bg-black">
-      <Navbar className="mt-4"></Navbar>
-      <Body />
-      <Features />
-      <Footer />
+      <SignIn />
     </div>
   );
 }
---
+++ frontend/src/components/Signin/SignInCard.tsx
@@ -0,0 +1,70 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+export default function SignIn() {
+  return (
+    <div className="place-items-center mt-40 justify-center">
+      <Card className="w-full max-w-sm mt-4 bg-black">
+        <div className="flex gap-4 items-center justify-center">
+          <img
+            className=""
+            width={50}
+            height={50}
+            src="https://img.icons8.com/sf-black-filled/64/FFFFFF/chat-message.png"
+          />
+          <h2 className="text-2xl text-white">ChatU</h2>
+        </div>{" "}
+        <CardHeader className="text-white"></CardHeader>
+        <CardContent className="text-white">
+          <form>
+            <div className="flex flex-col gap-8">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  className="mt-2"
+                />
+              </div>
+              <div className="grid gap-2 mt-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  className="mt-2"
+                  id="password"
+                  type="password"
+                  required
+                />
+                <a
+                  href="#"
+                  className="mt-2 text-left inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button
+            className="bg-gray-800 hover:bg-gray-700 w-full "
+            type="submit"
+          >
+            Login
+          </Button>
+          <h2 className="text-white text-md mt-4">
+            Don't have an account ?&nbsp;
+            <a className="text-white  cursor-pointer underline-offset-4 hover:underline">
+              SignUp
+            </a>
+          </h2>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
